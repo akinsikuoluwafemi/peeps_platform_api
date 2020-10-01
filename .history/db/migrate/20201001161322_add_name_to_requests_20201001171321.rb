4bd9d529ac4a38b90b37fa4b0d5d9844cffff168
Oluwafemi <akinsiku13@gmail.com>
2020-10-01 18:02:03 +0100

    Add name field to required params

---
+++ .history/db/migrate/20201001161322_add_name_to_requests_20201001171321.rb
@@ -0,0 +1,4 @@
+class AddNameToRequests < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_152141) do
+ActiveRecord::Schema.define(version: 2020_10_01_161322) do
 
   create_table "requests", force: :cascade do |t|
     t.text "description"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_09_25_152141) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
