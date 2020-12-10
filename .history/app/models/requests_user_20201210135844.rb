5696eb265debcb3e5dbcf1b5d728a52ba3d4dec7
Oluwafemi <akinsiku13@gmail.com>
2020-12-10 14:03:29 +0100

    delete unused migration file

---
+++ .history/app/models/requests_user_20201210135844.rb
@@ -0,0 +1,10 @@
+class RequestsUser < ApplicationRecord
+    belongs_to :user
+    belongs_to :request
+
+    validates :request_id, presence: true
+    validates :user_id, presence: true
+
+
+
+end
--- db/migrate/20201005130702_drop_requests_users.rb
+++
@@ -1,5 +0,0 @@
-class DropRequestsUsers < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :requests_users
-  end
-end
