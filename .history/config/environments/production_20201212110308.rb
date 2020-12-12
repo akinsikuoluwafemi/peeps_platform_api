971ecdcbded56ea53ca09e6b06aa81c847f73b0c
Oluwafemi <akinsiku13@gmail.com>
2020-12-12 11:11:32 +0100

    debug action cable production.rb file

--- .history/config/environments/production_20201212110308.rb
+++ .history/config/environments/production_20201212110308.rb
@@ -46,8 +46,8 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'http://peeps-platform.herokuapp.com' ]
-  # config.action_cable.disable_request_forgery_protection = true
+  # config.action_cable.allowed_request_origins = [ 'http://peeps-platform.herokuapp.com', /http:\/\/example.*/ ]
+  config.action_cable.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
--- .history/config/environments/production_20201212110925.rb
+++ .history/config/environments/production_20201212110925.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'http://peeps-platform.herokuapp.com' ]
+  config.action_cable.allowed_request_origins = ['http://peeps-platform.herokuapp.com' ]
   # config.action_cable.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
