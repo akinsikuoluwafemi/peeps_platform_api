392cd9de7659d45073311d4828946989bb4a74ac
Oluwafemi <akinsiku13@gmail.com>
2020-12-17 00:26:07 +0100

    debug actioncable url

--- .history/config/environments/production_20201217001745.rb
+++ .history/config/environments/production_20201217001745.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
 
 
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'https://peeps-frontend.vercel.app',  /https:\/\/peeps-frontend.*/ ]
+  # config.action_cable.allowed_request_origins = [ 'https://peeps-frontend.vercel.app',  /https:\/\/peeps-frontend.*/ ]
   # config.action_cable.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
