11dc7028b7277ef97b490ded5e9be669c3260c3e
Oluwafemi <akinsiku13@gmail.com>
2020-12-26 11:42:43 +0100

    delete unused scaffold

--- .history/config/environments/production_20201223220107.rb
+++ .history/config/environments/production_20201223220107.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
-  config.active_storage.service = :cloudinary
+  config.active_storage.service = :local
 
 
   # Mount Action Cable outside main process or domain.
--- test/models/request_user_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RequestUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
